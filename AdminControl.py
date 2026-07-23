f8f92eb399773669dfc0009ac803d7d76ce24351
HenryF <henryfabinski36@outlook.com>
2026-07-23 17:48:08 -0400

    Alowed Admin Control to use previous console app functions.

--- AdminControl.py
+++ AdminControl.py
@@ -28,7 +28,24 @@ def AdminControls():
     AdminWin.title("Python Storage")
     AdminWin.geometry("800x600")
     tk.Label(AdminWin, text="Admin Control").pack()
-    tk.Button(AdminWin, text="OLD SYSTEM", command=Executive).pack(pady=5)
+
+    ButtonScreen = tk.Frame(AdminWin)
+
+    ButtonScreen.pack()
+
+    def OpenDelStorage():
+        Executive("1")
+    def OpenExecCode():
+        Executive("2")
+    def OpenImpersonate():
+        Executive("3")
+
+
+    tk.Button(ButtonScreen, text="Clear Storage", command=OpenDelStorage).pack(pady=5)
+    tk.Button(ButtonScreen, text="Execute Code", command=OpenExecCode).pack(pady=5)
+    tk.Button(ButtonScreen, text="Impersonate", command=OpenImpersonate).pack(pady=5)
+
+
 
 
 
@@ -52,53 +69,38 @@ def AdminControls():
 
 
 # Old Console Version of program did this, so I will keep it for now
-def Executive():
-    pa = input("")
-    hashed = Hash(pa)
+def Executive(what):
     import os
-
-    if hashed == "f99bac0c814f3feb1325ef9698a2ad37dfad1694dab51fcc29ef4b445ce6fc74":
-        print("Hello, Henry, The Wise Creator who knows the secret code!\nExecutive Functions Active.")
-        print("1. Delete All Storage.\n2. Execute Python Code.\n3. Log in as someone else.\n4. Encrypt and turn to lowercase 50,000 times.")
-        what = input("Whitch Number?")
-
-        if what == "1": # Clear Storage
-            for ext in (".db", ".dat", ".dir"):
-                try:
-                    os.remove("Storage" + ext)
-                except FileNotFoundError:
-                    pass
-            print("All Storage Deleted.")
-            exit()
-        elif what == "2": # Execute Code
-            command = input("Be careful about executing code. It can destroy your system, or corupt all storage.\nDo control C or write c to cancel. \nBe careful! \nEnter Python code: ")
-            if command.lower() == "c":
-                print("Code not Executed. Cancelled. Bye, Henry!")
-                exit()
+    print("Hello, Henry, The Wise Creator who knows the secret code!\nExecutive Functions Active.")
+    #print("1. Delete All Storage.\n2. Execute Python Code.\n3. Log in as someone else.")
+    if what == "1": # Clear Storage
+        for ext in (".db", ".dat", ".dir"):
             try:
-                exec(command)
-                print("Code Executed. Bye, Henry!")
-                exit()
-            except Exception as e:
-                print("Code not Executed. Error:", e)
-                exit()
-        elif what == "3":   # Impersonate  
-            with shelve.open("Storage") as db:
-                print("Choose a user to inpersonate:")
-                for user in db.keys():
-                    print(user + ",")
-                n = input("What username:")
-                try:
-                    MainSystem(n.lower())
-                except:
-                    print("No user.")
-                exit()
-        elif what == "4":
-            pre = input("What? ")
-            slt = input("Salt? ")
-            print(Hash(pre, slt))
-        else:
-            exit()
-
-    else: # If no permision to access the backdoor, exit the program.
+                os.remove("Storage" + ext)
+            except FileNotFoundError:
+                pass
+        print("All Storage Deleted.")
         exit()
+    elif what == "2": # Execute Code
+        command = input("Be careful about executing code. It can destroy your system, or corupt all storage.\nDo control C or write c to cancel. \nBe careful! \nEnter Python code: ")
+        if command.lower() == "c":
+            print("Code not Executed. Cancelled. Bye, Henry!")
+            exit()
+        try:
+            exec(command)
+            print("Code Executed. Bye, Henry!")
+            exit()
+        except Exception as e:
+            print("Code not Executed. Error:", e)
+            exit()
+    elif what == "3":   # Impersonate  
+        with shelve.open("Storage") as db:
+            print("Choose a user to inpersonate:")
+            for user in db.keys():
+                print(user + ",")
+            n = input("What username:")
+            try:
+                MainSystem(n.lower())
+            except:
+                print("No user.")
+            exit()
